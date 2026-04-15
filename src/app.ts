5bcff65a447e0a5e3b0ca319650a8f074b46f32c
Xenopus19 <82256288+Xenopus19@users.noreply.github.com>
2026-04-15 13:24:33 +0300

    Client added

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,15 @@
 import express from "express";
 import usersRouter from "./controllers/users";
 import loginRouter from "./controllers/login";
+import cors from 'cors';
 const app = express();
 
 app.use(express.json())
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true
+}))
 
-app.get("/", (req, res) => {
-  res.send("App");
-});
 
 app.use("/api/users", usersRouter);
 app.use("/api/login", loginRouter);
